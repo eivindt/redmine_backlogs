b5771231e1109c1207e27a9ce103ffa30a680d76
Eivind Tagseth <eivindt@norbit.no>
2026-09-16 13:09:52 +0200

    Fix correct positioning of task creation popup

--- lib/backlogs_activerecord_mixin.rb
+++ lib/backlogs_activerecord_mixin.rb
@@ -168,7 +168,7 @@ module Backlogs
       end
 
       def list_commit
-        self.class.connection.execute("update #{self.class.table_name} set position = #{self.position} where id = #{self.id}") unless self.new_record?
+	self.class.connection.execute("update #{self.class.table_name} set position = #{self.position} where id = #{self.id}") unless self.new_record? or self.position.blank?
         #FIXME now the cached lower/higher_item are wrong during this request. So are those from our old and new peers.
       end
 
